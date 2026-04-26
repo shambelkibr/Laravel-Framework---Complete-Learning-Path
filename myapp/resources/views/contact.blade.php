f5e41fead8cf2e463a874e42a4bcd6da136c80cc
shanbelkibr <shambel5110@gmail.com>
2026-04-27 02:15:12 +0300

    day3 and 4 practice

---
+++ myapp/resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>this is contact page </p>
+    <h1>Contact me at shambel5110@gmail.com</h1>
+</body>
+</html>
--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "Hello! This is my first Laravel page.";
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
