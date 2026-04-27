443acd63f90c96be39073c86eb352c5626e29fa1
ShanbelKibre <shambel5110@gmail.com>
2026-04-27 16:31:33 +0300

    add about controller with daynamic valuse

--- myapp/app/Http/Controllers/PageController.php
+++ myapp/app/Http/Controllers/PageController.php
@@ -8,15 +8,25 @@ class PageController extends Controller
 {
     //
 
-    public function home()
-    {
-        return view('welcome');
-    }
+  public function home()
+{
+    $siteName = "My Laravel Learning Site";
+    $currentYear = date('Y');
+    
+    return view('welcome', [
+        'siteName' => $siteName,
+        'currentYear' => $currentYear
+    ]);
+}
 
-    public function about()
-    {
-        return view('about');
-    }
+   public function about()
+{
+    $author = "Shanbel Kibre";
+    $learningDays = 6;  // You're on Day 6!
+    $goal = "Master Laravel from scratch";
+    
+    return view('about', compact('author', 'learningDays', 'goal'));
+}
 
     public function contact()
     {
--- myapp/resources/views/about.blade.php
+++ myapp/resources/views/about.blade.php
@@ -6,5 +6,7 @@
 <body>
     <h1>About Me</h1>
     <p>I am learning Laravel from scratch, one step at a time.</p>
+    <p>my name is {{ $author }} and i am learning {{ $learningDays }} and my goal is {{ $goal }}</p>
+
 </body>
 </html>
